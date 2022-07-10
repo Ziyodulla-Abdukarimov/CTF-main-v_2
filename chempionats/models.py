9df36373bbd61c8d9c242a23a58a0c7d6fc61d5c
Abdukarimov Ziyodulla <ZiyodullaDeveloper@gmail.com>
2022-07-10 18:00:27 +0000

    a

--- chempionats/models.py
+++ chempionats/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import Client
 
 # Create your models here.
 class Chempionats(models.Model):
@@ -11,4 +12,25 @@ class Chempionats(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title    
+        return self.title
+
+
+class Chempionat_task(models.Model):
+    chempstitle = models.ForeignKey(Chempionats, on_delete=models.CASCADE, related_name='chemptask')
+    title = models.CharField(max_length=100, verbose_name='Mavzu')
+    body = models.CharField(max_length=1000, verbose_name='savol')
+    text = models.CharField(max_length=100, blank=True, null=True)
+    link = models.CharField(max_length=500, blank=True, null=True)
+    flag = models.CharField(max_length=50)
+    point = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Chempionat_user(models.Model):
+    user = models.ForeignKey(Client, on_delete=models.CASCADE)
+    point = models.IntegerField(default=0)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True)
