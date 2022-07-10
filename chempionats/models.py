e48839c2efd31ef474f7bd3774898b342cfa81b1
Abdukarimov Ziyodulla <ZiyodullaDeveloper@gmail.com>
2022-07-10 17:21:12 +0000

    g

--- chempionats/models.py
+++ chempionats/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Chempionats(models.Model):
+    title = models.CharField(max_length=70)
+    description = models.CharField(max_length=1000)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title    
