a5b26fe72a1821a9ff469b35fd11f7bb61dfb6e0
jam <jamshidbekollanazarov@outlook.com>
2022-07-12 01:52:09 -0400

    error tuzatildi va comment model yaratilib migrate qilinda va admin panelga izohlar bolimi qoshilib ishlayabdi

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Blogs
+from .models import Blogs, Comment
 # Register your models here.
 
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+class BlogsAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+
+admin.site.register(Comment)
 admin.site.register(Blogs)
--- blogs/models.py
+++ blogs/models.py
@@ -18,3 +18,10 @@ class Blogs(models.Model):
         return self.title
     def get_absolute_url(self):
         return reverse('article_detail',args=[str(self.id)])
+
+class Comment(models.Model):
+        blog = models.ForeignKey(Blogs, on_delete=models.CASCADE, related_name='comments')
+        comment  = models.CharField(max_length=150)
+        author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+        def __str__(self):return self.comment
+        def get_absolute_url(self):return reverse('blogs_list')
