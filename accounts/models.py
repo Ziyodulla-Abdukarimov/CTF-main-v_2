90bb59302189adc6a41993b1038b337b9c88db15
Abdukarimov Ziyodulla <ZiyodullaDeveloper@gmail.com>
2022-07-17 06:40:41 +0000

    model qo'shildi

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,7 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.forms import CharField
 
 # Create your models here.
 
