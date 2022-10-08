1ed64f6e0ea34ed3b092ac6b4bb7b044ba6e0afb
Ziyodulla-Abdukarimov <ZiyodullaDeveloper@gmail.com>
2022-10-08 14:02:19 -0500

    color edited

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2i96-8%-xpe)t@ew8p#t24^1e&zl!)0rh^7r@dcaa^lmgtl$8e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.5']
 
 
 # Application definition
